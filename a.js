1f26646dfade1987951833dff62567f86d6de89e
Oscar <oscar99emilios@gmail.com>
2022-05-05 23:24:15 -0500

    Parte de JS terminada, falta mejorar estilo css

--- a.js
+++ a.js
@@ -1,7 +1,56 @@
-let boton = document.getElementsByClassName("boton")
 
+const encriptado = document.getElementById("c")
+
+
+function btnEncriptar(){
+    // document.getElementById("c").value = document.getElementById("d").value
+    
+    document.getElementById("c").value = encritar(document.getElementById("d").value)
+}
+
+function btnDesencriptar(){
+    document.getElementById("c").value = desencritar(document.getElementById("d").value)
+}
 
 
 function disparar() {
     document.getElementById("a").style.display="none";
+    document.getElementById("b").style.display="block";
+    btnEncriptar()
+
 }
+
+function disparar1  () {
+    document.getElementById("a").style.display="none";
+    document.getElementById("b").style.display="block";
+    btnDesencriptar()
+
+}
+
+
+function encritar(texto){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    texto = texto.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(texto.includes(matrizCodigo[i][0])){
+            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return texto  
+
+}
+
+function desencritar(texto){
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]]
+    texto = texto.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(texto.includes(matrizCodigo[i][0])){
+            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return texto  
+
+}
+
+// funcion para resgrigir 
+
